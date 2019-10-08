628df43b1aecbf2e9669cca98d63f8f9d3bd2853
Miran Jang <miranjang9507@gmail.com>
2019-10-08 18:19:13 -0400

    update

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,3 +3,13 @@ var express = require("express");
 var router = express.Router();
 
 var burger = require("../models/burger.js");
+
+router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+       
+    });
+});
+
+router.post("/", function(req, res) {
+    burger.insertOne("")
+})
--- models/burger.js
+++ models/burger.js
@@ -1,2 +1,23 @@
 var orm = require("../config/orm.js");
 
+var burger = {
+    selectAll: function(cb) {
+        orm.selectAll("burger", function(res) {
+            cb(res);
+        });
+    },
+
+    insertOne: function(cols, vals, cb) {
+        orm.insertOne("burger", cols, vals, function(res) {
+            cb(res);
+        })
+    },
+
+    updateOne: function(obj, condition, cb) {
+        orm.updateOne("burger", obj, condition, function(res) {
+            cb(res);
+        });
+    }
+};
+
+module.exports = burger;
