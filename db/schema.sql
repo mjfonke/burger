dfa2f8246091f96f688cc4a5956bd3f00ce51cda
Miran Jang <miranjang9507@gmail.com>
2019-10-10 23:01:53 -0400

    design edited

--- db/schema.sql
+++ db/schema.sql
@@ -7,7 +7,7 @@ use burgers_db;
 create table burgers (
     id integer auto_increment not null,
     burger_name varchar(100),
-    devoured boolean,
+    devoured boolean default false,
     primary key (id)
 );
 
